529589c85287948107fdd05b6a5b37d06800ea46
dr3amy <arterterone@gmail.com>
2019-12-16 19:38:26 +0300

    switched to has_and_belongs_to_many

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   validates :user_id, presence: true
   belongs_to :user
-  has_many :products, through: :orders_products
+  has_and_belongs_to_many :products
 end
 
