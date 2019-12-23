b440b6384c5213e48c88613e32c63d635ccf021d
dr3amy <arterterone@gmail.com>
2019-12-24 00:14:33 +0300

    added devise for users and admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products
   resources :sizes, only: [:index]
-  root 'welcome#index'
+  root to: 'welcome#index'
+
 end
