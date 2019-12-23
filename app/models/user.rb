768bc59c1f84b27861b3acfa968bf52505647390
dr3amy <arterterone@gmail.com>
2019-12-24 00:12:41 +0300

    added devise props

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :email, :password, :town, :country, :address_line1, :postcode, presence: true
   has_many :orders
 end
