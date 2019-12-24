0017c5bc77f5259c9576821aaa935def9aff10f9
dr3amy <arterterone@gmail.com>
2019-12-25 01:34:33 +0300

    added oauth for github [ISSUE]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :trackable,
+         :validatable,
          :omniauthable, :omniauth_providers => [:github]
 
   validates :email, :password, :town, :country, :address_line1, :postcode, presence: true
