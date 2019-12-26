9f3bdd7effb76a2b8c66e6b34abda4f3f22e6b72
dr3amy <arterterone@gmail.com>
2019-12-26 21:36:55 +0300

    removed :trackable state

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
-         :validatable,
+         :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:github]
 
   validates :email, :password, :town, :country, :address_line1, :postcode, presence: true
